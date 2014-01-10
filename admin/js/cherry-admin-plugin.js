46a5feb3af9b755973aef869e4cb7ba98ba9e544
cheh <chehovskiy.dima@gmail.com>
2014-01-11 00:16:49 +0200

    создал виджет Post Cycle (my-post-cycle-widget.php)

--- admin/js/cherry-admin-plugin.js
+++ admin/js/cherry-admin-plugin.js
@@ -1,5 +1,8 @@
-/*jQuery(document).ready(function(){
-
-
+jQuery(document).ready(function(){
+	// ---------------------------------------------------------
+	// Ajax Filter
+	// ---------------------------------------------------------
+	jQuery('#toolbar-filter select').live('change', function(e){
+		load_filters(this);
+	});
 })
-*/
--- includes/plugin-script.php
+++ includes/plugin-script.php
@@ -2,7 +2,10 @@
 //include stylesheet files
 	if ( !function_exists('cherry_include_stylesheet') ) {
 		function cherry_include_stylesheet() {
-			wp_enqueue_style( 'cherry-plugin', CHERRY_PLUGIN_URL . 'includes/css/cherry-plugin.css', array(), CHERRY_PLUGIN_VERSION, 'all' );
+			wp_enqueue_style( 'cherry-plugin', CHERRY_PLUGIN_URL . 'includes/css/cherry-plugin.css', false, CHERRY_PLUGIN_VERSION, 'all' );
+			wp_deregister_style( 'flexslider' );
+			wp_register_style( 'flexslider', CHERRY_PLUGIN_URL . 'lib/FlexSlider/flexslider.css', false, '2.0', 'all' );
+			wp_enqueue_style( 'flexslider' );
 		}
 		add_action( 'wp_enqueue_scripts', 'cherry_include_stylesheet', 9);
 	}
@@ -11,6 +14,9 @@
 	if ( !function_exists('cherry_include_script') ) {
 		function cherry_include_script(){
 			wp_enqueue_script( 'cherry-plugin', CHERRY_PLUGIN_URL . 'includes/js/cherry-plugin.js', array('jquery'), CHERRY_PLUGIN_VERSION, true );
+			wp_deregister_script( 'flexslider' );
+			wp_register_script( 'flexslider', CHERRY_PLUGIN_URL . 'lib/FlexSlider/jquery.flexslider-min.js', array('jquery'), '2.1', true );
+			wp_enqueue_script( 'flexslider' );
 		}
 		add_action( 'wp_enqueue_scripts', 'cherry_include_script' );
 	}
