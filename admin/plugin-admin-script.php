242983035c47a9a1a5a13909288c5287bc203d57
Grauen <rolli@reccsoft.com>
2014-08-21 00:45:34 +0200

    removed http-prefix
    removed http-prefix so admins can use ssl properly

--- admin/plugin-admin-script.php
+++ admin/plugin-admin-script.php
@@ -10,7 +10,7 @@
 	if(!function_exists('cherry_include_admin_style')){
 		function cherry_include_admin_style(){
 			wp_enqueue_style('cherry_plugin_stylesheet', CHERRY_PLUGIN_URL.'admin/css/cherry-admin-plugin.css', false, '0.1', 'all');
-			wp_enqueue_style('font-awesome', 'http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css', false, '3.2.1', 'all');
+			wp_enqueue_style('font-awesome', '//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css', false, '3.2.1', 'all');
 		}
 		add_action( 'admin_enqueue_scripts', 'cherry_include_admin_style' );
 	}
