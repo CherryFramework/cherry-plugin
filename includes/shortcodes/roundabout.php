444779f1e8e7b28544e82caf7faa6fed90fc6830
cheh <chehovskiy.dima@gmail.com>
2015-03-03 23:48:53 +0200

    Fixed roundabout shortcode in home page

--- includes/shortcodes/roundabout.php
+++ includes/shortcodes/roundabout.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * Roundabout
- *
+ * Roundabout shortcode.
  */
-if (!function_exists('shortcode_roundabout')) {
+if ( !function_exists( 'shortcode_roundabout' ) ) {
 	function shortcode_roundabout( $atts, $content = null, $shortcodename = '' ) {
-		extract(shortcode_atts(array(
+		extract( shortcode_atts( array(
 			'title'            => '',
 			'num'              => '3',
 			'type'             => '',
@@ -15,43 +14,50 @@ if (!function_exists('shortcode_roundabout')) {
 			'custom_category'  => '',
 			'more_button_text' => '',
 			'more_button_link' => '',
-			'custom_class'     => ''
-		), $atts));
+			'custom_class'     => '',
+		), $atts ) );
 
-		wp_enqueue_script( 'roundabout_script', CHERRY_PLUGIN_URL . 'lib/js/roundabout/jquery.roundabout.min.js', array('jquery') );
-		wp_enqueue_script( 'roundabout_shape', CHERRY_PLUGIN_URL . 'lib/js/roundabout/jquery.roundabout-shapes.min.js', array('jquery') );
+		wp_enqueue_script( 'roundabout_script', CHERRY_PLUGIN_URL . 'lib/js/roundabout/jquery.roundabout.min.js', array( 'jquery' ), '1.0' );
+		wp_enqueue_script( 'roundabout_shape', CHERRY_PLUGIN_URL . 'lib/js/roundabout/jquery.roundabout-shapes.min.js', array( 'jquery' ), '1.0' );
 
-		$ra_id = uniqid();
+		$ra_id       = uniqid();
 		$itemcounter = 0;
-		// check what type of post user selected
-		switch ($type) {
+
+		// Check what type of post user selected.
+		switch ( $type ) {
 			case 'blog':
 				$type_post = 'post';
 				break;
+
 			case 'portfolio':
 				$type_post = 'portfolio';
 				break;
+
 			case 'testimonial':
 				$type_post = 'testi';
 				break;
+
 			case 'our team':
 				$type_post = 'team';
 				break;
+
 			default:
 				$type_post = 'post';
 				break;
 		}
 
-		$output = '<div class="roundabout-holder '.$custom_class.'">';
-		if ($title != '') {
-			$output .= '<h2>'.$title.'</h2>';
+		$output = '<div class="roundabout-holder ' . $custom_class . '">';
+
+		if ( $title != '' ) {
+			$output .= '<h2>' . $title . '</h2>';
 		}
-		$output .= '<ul id="roundabout-list-'.$ra_id.'" class="unstyled">';
+
+		$output .= '<ul id="roundabout-list-' . $ra_id . '" class="unstyled">';
 
 		global $post;
 
-		// WPML filter
-		$suppress_filters = get_option('suppress_filters');
+		// WPML filter.
+		$suppress_filters = get_option( 'suppress_filters' );
 
 		$args = array(
 			'post_type'              => $type_post,
@@ -60,36 +66,40 @@ if (!function_exists('shortcode_roundabout')) {
 			'posts_per_page'         => -1,
 			'orderby'                => 'post_date',
 			'order'                  => 'DESC',
-			'suppress_filters'       => $suppress_filters
+			'suppress_filters'       => $suppress_filters,
 		);
 
-		$posts = get_posts($args);
-		$i = 1;
+		$posts = get_posts( $args );
+		$i     = 1;
 
-		foreach($posts as $key => $post) {
-			//Check if WPML is activated
+		foreach ( $posts as $key => $post ) {
+
+			// Check if WPML is activated.
 			if ( defined( 'ICL_SITEPRESS_VERSION' ) ) {
 				global $sitepress;
 
 				$post_lang = $sitepress->get_language_for_element($post->ID, 'post_' . $type_post);
 				$curr_lang = $sitepress->get_current_language();
-				// Unset not translated posts
+
+				// Unset not translated posts.
 				if ( $post_lang != $curr_lang ) {
 					unset( $posts[$key] );
 				}
-				// Post ID is different in a second language Solution
+
+				// Post ID is different in a second language Solution.
 				if ( function_exists( 'icl_object_id' ) ) {
 					$post = get_post( icl_object_id( $post->ID, $type_post, true ) );
 				}
 			}
-			setup_postdata($post);
+
+			setup_postdata( $post );
 
 			if ( $num == -1 ) {
-				$num = count($posts);
+				$num = count( $posts );
 			}
 
 			if ( $num >= $i ) {
-				if (has_post_thumbnail($post->ID)) {
+				if ( has_post_thumbnail( $post->ID ) ) {
 					$attachment_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
 					$url            = $attachment_url['0'];
 					$image          = aq_resize($url, $thumb_width, $thumb_height, true);
@@ -103,7 +113,9 @@ if (!function_exists('shortcode_roundabout')) {
 				}
 			}
 		}
-		wp_reset_postdata(); // restore the global $post variable
+
+		// Restore the global $post variable.
+		wp_reset_postdata();
 		$output .= '</ul>';
 
 		if (($more_button_text != '') && ($more_button_link != '')) {
@@ -119,13 +131,13 @@ if (!function_exists('shortcode_roundabout')) {
 						shape: "square",
 						responsive: true
 					});
-				});
-				jQuery(window).bind("resize", function() {
-					jQuery("#roundabout-list-'.$ra_id.' li").removeAttr("style");
-					jQuery("#roundabout-list-'.$ra_id.'").roundabout({
-						minOpacity: 1,
-						minScale: 0.6,
-						responsive: false
+					jQuery(window).bind("resize", function() {
+						jQuery("#roundabout-list-'.$ra_id.' li").removeAttr("style");
+						jQuery("#roundabout-list-'.$ra_id.'").roundabout({
+							minOpacity: 1,
+							minScale: 0.6,
+							responsive: false
+						});
 					});
 				});';
 		$output .= '</script>';
@@ -134,6 +146,6 @@ if (!function_exists('shortcode_roundabout')) {
 		$output = apply_filters( 'cherry_plugin_shortcode_output', $output, $atts, $shortcodename );
 
 		return $output;
-}
-	add_shortcode('roundabout', 'shortcode_roundabout');
-}?>
+	}
+	add_shortcode( 'roundabout', 'shortcode_roundabout' );
+} ?>
